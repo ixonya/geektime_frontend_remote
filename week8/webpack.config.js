5677ef83b9f231b19e7a676a23fa27c090fa524c
Galen Yao <galen.yao@oracle.com>
2021-06-13 22:17:21 +0800

    homework of week 8

---
+++ week8/webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    entry: "./main.js",
+    output: {
+        filename:"bundle.js",
+        path: __dirname + '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                        plugins: [
+                            [
+                                "@babel/plugin-transform-react-jsx",
+                                {
+                                    pragma: "createElement"
+                                }
+                            ]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    mode: "development"
+}
